2faa033110bc33a81f4a986ce4d2811cec40c8e0
Ranjit Parmar <parmarranjit7972@gmail.com>
2024-09-24 19:45:33 +0530

    modified filed added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -44,7 +44,7 @@ export const createUser = asyncErrorHandler(async (req, res, next) => {
         expires: new Date(
           Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
         ),
-        // httpOnly: true,
+        httpOnly: true,
       };
     
 
@@ -94,7 +94,7 @@ export const LoginUser = asyncErrorHandler(async (req, res, next) => {
         expires: new Date(
             Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
         ),
-        // httpOnly: true,
+        httpOnly: true,
       };
     
       
