735a0d36822f1598e776ec41fa96dbbe702a5290
Ranjit Parmar <parmarranjit7972@gmail.com>
2024-09-27 20:55:42 +0530

    all required changes applied

--- src/app.js
+++ src/app.js
@@ -13,8 +13,6 @@ import paymentRoute from './routes/paymentRoute.js';
 import {connectDb} from './utils/conn.js'
 import globalErrorHandler from './middlewares/errorController.js'
 import path from 'path';
-import  {customError}  from './utils/customError.js';
-import asyncErrorHandler from './utils/asyncErrorHandler.js';
 import { v2 as cloudinary } from 'cloudinary';
 
 process.on("uncaughtException",(err)=>{
@@ -43,39 +41,10 @@ app.use(
     cors({
         
         origin: ["https://shoppingecommerce-omega.vercel.app"],
-        // origin:  "https://localhost:5173",
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
     })
 );
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://shoppingecommerce-omega.vercel.app"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   res.setHeader("Access-Control-Allow-Private-Network", true);
-//   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-//   res.setHeader("Access-Control-Max-Age", 7200);
-
-//   next();
-// });
-
-// app.use(cors({
-//   // origin: "https://shopping-app-three-ashy.vercel.app",
-//   origin: "https://shoppingecommerce-omega.vercel.app",
-//   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//   allowedHeaders: ['Content-Type', 'Authorization'], 
-//   credentials: true,
-//   }))
 
 const __dirname = path.resolve(); 
 app.use("/uploads",express.static("uploads"));
@@ -98,11 +67,6 @@ app.use('/api/v1/coupon', couponRoute);
 //  payment routing
 app.use('/api/v1/payment', paymentRoute);
 
-// app.all("*",asyncErrorHandler(async(req,res,next)=>{
-//     const err = new customError(`can't find ${req.originalUrl} on the server`,404);
-//     next(err);
-// }))
-
 app.use(express.static(path.join(__dirname, "../ecommerce_frontend/build")));
 
 app.get("*", (req, res) => {
@@ -110,7 +74,6 @@ app.get("*", (req, res) => {
 });
 
 app.use(globalErrorHandler);
-
 
 app.listen(port, ()=>{
     console.log("server is running on port ",port);
